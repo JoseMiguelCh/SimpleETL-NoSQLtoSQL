ef2675ffb0ce1f42d7c0bdd13afe76c970d1442d
Jose Miguel Chacón <josemiguelchacon@outlook.com>
2024-06-06 18:38:58 -0500

    Test custom date range plus some comments

--- migracionetls/data_extraction/extract.py
+++ migracionetls/data_extraction/extract.py
@@ -1,3 +1,7 @@
+"""
+Extract data from Cosmos DB
+"""
+# pylint: disable=import-error
 import os
 from dotenv import load_dotenv
 
@@ -7,19 +11,33 @@ MASTER_KEY = os.getenv("MASTER_KEY")
 DATABASE_ID = os.getenv("DATABASE_ID")
 
 def load_cosmos_config(container):
-    cosmosConfig = {
+    """
+    Load cosmos db configuration
+    """
+    cosmos_config = {
         "spark.cosmos.accountEndpoint": ENDPOINT,
         "spark.cosmos.accountKey": MASTER_KEY,
         "spark.cosmos.database": DATABASE_ID,
         "spark.cosmos.container": container
     }
-    return cosmosConfig
+    return cosmos_config
 
-def extract_data(spark, container, schema):
-    cosmosConfig = load_cosmos_config(container)
-    df = spark.read.format("cosmos.oltp") \
-        .options(**cosmosConfig) \
-        .schema(schema) \
-        .load()
+def extract_data(spark, container, schema, date_range=None):
+    """
+    Extract data from cosmos db
+    """
+    cosmos_config = load_cosmos_config(container)
+    if date_range:
+        df = spark.read.format("cosmos.oltp") \
+            .options(**cosmos_config) \
+            .schema(schema) \
+            .option("spark.cosmos.read.partitioning.strategy", "Restrictive") \
+            .option("spark.cosmos.read.inferSchema.enabled", "true") \
+            .load(f"SELECT * FROM c WHERE c.fechaHora BETWEEN '{date_range[0]}' AND '{date_range[1]}'")
+    else:
+        df = spark.read.format("cosmos.oltp") \
+            .options(**cosmos_config) \
+            .schema(schema) \
+            .load()
 
     return df
--- migracionetls/main.py
+++ migracionetls/main.py
@@ -1,3 +1,7 @@
+"""
+Main module to run the ETL process. It extracts data from Cosmos DB, transforms it and loads it into a Postgres database.
+"""
+# pylint: disable=import-error
 import os
 import logging
 from dotenv import load_dotenv
@@ -14,24 +18,29 @@ ENDPOINT = os.getenv("ENDPOINT")
 MASTER_KEY = os.getenv("MASTER_KEY")
 
 CONTAINERS_TO_EXTRACT = {
-    'CodigosEPCHab': codigos_epc_hab_map,
-    'ListaUsuariosHab': lista_usuarios_hab_map,
-    'NegacionesPasoHab': negaciones_paso_hab_map,
-    'PruebasHab': pruebas_hab_map,
-    'PasosHab': pasos_hab_map
+    #'CodigosEPCHab': codigos_epc_hab_map,
+    #'ListaUsuariosHab': lista_usuarios_hab_map,
+    #'NegacionesPasoHab': negaciones_paso_hab_map,
+    #'PruebasHab': pruebas_hab_map,
+    'PasosNew': pasos_hab_map
 }
+date_range = ("2024-05-20T00:00:00Z", "2024-05-20T00:05:00Z")
 
 def main():
+    """
+    Main function to run the ETL process
+    """
     logging.info("------ Initializing spark session -------")
     spark = initialize_spark(ENDPOINT, MASTER_KEY)
-    for container in CONTAINERS_TO_EXTRACT:
-        logging.info(f"------ Starting extraction for {container} -------")
+    for container in CONTAINERS_TO_EXTRACT.items():
+        logging.info("------ Starting extraction for %s -------", container)
         logging.info("------ Extracting data from cosmos db -------")
-        df = extract_data(spark, container, CONTAINERS_TO_EXTRACT[container]['schema'])
+        df = extract_data(spark, container, CONTAINERS_TO_EXTRACT[container]['schema'], date_range)
         logging.info("------ Transforming data -------")
         transformed_data = transform_data(spark, df, CONTAINERS_TO_EXTRACT[container])
         logging.info("------ Loading data into PSQL -------")
-        [load_data(source, target) for source, target in transformed_data]
+        for source, target in transformed_data:
+            load_data(target, source)
     
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
