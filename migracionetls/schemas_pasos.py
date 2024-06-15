0571e34d24cecc1a9b133fd9e29f36fc1dc531bc
Jose Miguel Chacón <josemiguelchacon@outlook.com>
2024-06-15 18:34:00 -0500

    Refactor transform_data function for readability and performance

--- migracionetls/schemas_pasos.py
+++ migracionetls/schemas_pasos.py
@@ -14,10 +14,10 @@ confirmaciones_procesamiento_schema = ArrayType(StructType([
     StructField("auditoria", StructType([
         StructField("solicitante", IntegerType(), True),
         StructField("codigoCorrelacion", StringType(), True),
-        StructField("fechaHoraServidor", TimestampType(), True),
+        StructField("fechaHoraServidor", StringType(), True),
         StructField("respondedor", IntegerType(), True),
-        StructField("tiempoInicio", TimestampType(), True),
-        StructField("tiempoFin", TimestampType(), True),
+        StructField("tiempoInicio", StringType(), True),
+        StructField("tiempoFin", StringType(), True),
         StructField("idPrueba", IntegerType(), True)
     ]), True),
     StructField("id", StringType(), True)
@@ -60,8 +60,8 @@ pasos_schema = StructType([
     StructField("id", StringType(), True),
     StructField("codigoPaso", StringType(), True),
     StructField("codigoOperador", IntegerType(), True),
-    StructField("fechaHora", TimestampType(), True),
-    StructField("fechaRecaudo", TimestampType(), True),
+    StructField("fechaHora", StringType(), True),
+    StructField("fechaRecaudo", StringType(), True),
     StructField("codigoCliente", StringType(), True),
     StructField("placa", StringType(), True),
     StructField("tid", StringType(), True),
@@ -77,13 +77,13 @@ pasos_schema = StructType([
     StructField("sentido", StringType(), True),
     StructField("placaOCR", StringType(), True),
     StructField("existeDescrepancia", BooleanType(), True),
-    StructField("fechaActualizacionUsuario", TimestampType(), True),
+    StructField("fechaActualizacionUsuario", StringType(), True),
     StructField("codigoListaUsuario", StringType(), True),
     StructField("existeDiscrepanciaPlaca", BooleanType(), True),
     StructField("confirmacionesProcesamiento", confirmaciones_procesamiento_schema, True),
     StructField("ajustes", ajustes_pasos_schema, True),
     StructField("auditoria", pasos_auditoria_schema, True),
-    StructField("_ts", LongType(), True),
+    StructField("_ts", IntegerType(), True),
 ])
 
 pasos_map = {
