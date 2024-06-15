1f2bd0d31e54d51645d6cc02b371929b56c9e0f0
Jose Miguel Chacón <josemiguelchacon@outlook.com>
2024-06-15 17:05:15 -0500

    chore: Refactor transform_data function for readability and performance

--- migracionetls/data_transformation/transform.py
+++ migracionetls/data_transformation/transform.py
@@ -1,11 +1,19 @@
+"""
+    Transform from nested to flat structure
+"""
+# pylint: disable=import-error
 from pyspark.sql.functions import col, explode, explode_outer
 from pyspark.sql.types import StructType, ArrayType
 
 def transform_data(spark, df, container_map):
+    """
+    Transform the data from a nested structure to a flat structure
+    """
     items = []
-    main_df = spark.createDataFrame(spark.sparkContext.emptyRDD(), container_map.get('schema'))
-    
+    #main_df = spark.createDataFrame(spark.sparkContext.emptyRDD(), container_map.get('schema'))
     details = container_map.get('details')
+    # Capitalize all column names
+    df = df.select([col(c).alias(c.upper()) for c in df.columns])
     if details is not None:
         for detail in details:
             detail_column_name = detail['column_name']
@@ -13,9 +21,9 @@ def transform_data(spark, df, container_map):
 
             column_type = df.schema[detail_column_name].dataType
             if isinstance(column_type, StructType):
-                detail_df = df.select("id", f"{detail_column_name}.*")
+                detail_df = df.select("Id", f"{detail_column_name}.*")
             elif isinstance(column_type, ArrayType):
-                join_key = detail.get('join_key', 'id')
+                join_key = detail.get('join_key', 'Id')
                 detail_df = expand_array_into_struct(df, join_key, detail_column_name)
             else:
                 raise ValueError(f"Unsupported column type for {detail_column_name}")
@@ -25,8 +33,7 @@ def transform_data(spark, df, container_map):
                 items.append((detail_df, detail_destination_table_name))
                 items.append((detail_auditoria, "auditoria_" + detail_destination_table_name))
             else:
-                items.append((detail_df, detail_destination_table_name))
-            
+                items.append((detail_df, detail_destination_table_name))      
     destination_table_name = container_map['destination_table_name']
     has_auditoria = container_map['has_auditoria']
     columns_to_drop = [detail['column_name'] for detail in details] if details else []
@@ -42,11 +49,17 @@ def transform_data(spark, df, container_map):
     return items
 
 def get_auditoria_df(df):
+    """
+    Split the auditoria column into a separate dataframe
+    """
     main_df = df.drop("auditoria")
-    auditoria_df = df.select("id", "auditoria.*")
+    auditoria_df = df.select("Id", "auditoria.*")
     return main_df, auditoria_df
 
 def expand_array_into_struct(df, join_key, array_column_name):
+    """
+    Expand an array column into a struct column
+    """
     df = df.withColumn(array_column_name, explode(col(array_column_name)))
     df = df.select(join_key, f"{array_column_name}.*")
     return df
---
+++ migracionetls/schemas_pasos.py
@@ -0,0 +1,109 @@
+"""
+    Schema definitions for 'pasos' collections
+"""
+# pylint: disable=import-error
+from pyspark.sql.types import StructType, StructField, StringType, BooleanType, IntegerType, TimestampType, ArrayType, LongType
+
+# Pasos hab
+confirmaciones_procesamiento_schema = ArrayType(StructType([
+    # StructField("codigoPaso", StringType(), True),
+    StructField("codigoIntermediador", IntegerType(), True),
+    StructField("tokenConfirmacion", StringType(), True),
+    StructField("codigoRespuesta", IntegerType(), True),
+    StructField("descripcion", StringType(), True),
+    StructField("auditoria", StructType([
+        StructField("solicitante", IntegerType(), True),
+        StructField("codigoCorrelacion", StringType(), True),
+        StructField("fechaHoraServidor", TimestampType(), True),
+        StructField("respondedor", IntegerType(), True),
+        StructField("tiempoInicio", TimestampType(), True),
+        StructField("tiempoFin", TimestampType(), True),
+        StructField("idPrueba", IntegerType(), True)
+    ]), True),
+    StructField("id", StringType(), True)
+]), True)
+
+pasos_auditoria_schema = StructType([
+    StructField("solicitante", IntegerType(), True),
+    StructField("codigoCorrelacion", StringType(), True),
+    StructField("fechaHoraServidor", TimestampType(), True),
+    StructField("respondedor", IntegerType(), True),
+    StructField("tiempoInicio", TimestampType(), True),
+    StructField("tiempoFin", TimestampType(), True),
+    StructField("idPrueba", IntegerType(), True)
+])
+
+ajustes_pasos_schema = ArrayType(StructType([
+    StructField("id", StringType(), True),
+    StructField("codigoAjuste", StringType(), True),
+    StructField("codigoOperador", IntegerType(), True),
+    StructField("fechaHora", StringType(), True),
+    StructField("fechaRecaudo", StringType(), True),
+    StructField("codigoPasoReferencia", StringType(), True),
+    StructField("motivoAjuste", IntegerType(), True),
+    StructField("valorAjuste", LongType(), True),
+    StructField("valor", LongType(), True),
+    StructField("categoria", StringType(), True),
+    StructField("tipoOperacion", IntegerType(), True),
+    StructField("auditoria", StructType([
+        StructField("solicitante", IntegerType(), True),
+        StructField("codigoCorrelacion", StringType(), True),
+        StructField("fechaHoraServidor", StringType(), True),
+        StructField("respondedor", IntegerType(), True),
+        StructField("tiempoInicio", StringType(), True),
+        StructField("tiempoFin", StringType(), True),
+        StructField("idPrueba", IntegerType(), True)
+    ]), True)
+]), True)
+
+pasos_schema = StructType([
+    StructField("id", StringType(), True),
+    StructField("codigoPaso", StringType(), True),
+    StructField("codigoOperador", IntegerType(), True),
+    StructField("fechaHora", TimestampType(), True),
+    StructField("fechaRecaudo", TimestampType(), True),
+    StructField("codigoCliente", StringType(), True),
+    StructField("placa", StringType(), True),
+    StructField("tid", StringType(), True),
+    StructField("epc", StringType(), True),
+    StructField("estacion", StringType(), True),
+    StructField("carril", StringType(), True),
+    StructField("valor", IntegerType(), True),
+    StructField("cantidadEjes", IntegerType(), True),
+    StructField("cantidadDobleRuedas", IntegerType(), True),
+    StructField("categoriaDAC", StringType(), True),
+    StructField("categoriaCobrada", StringType(), True),
+    StructField("tipoLectura", IntegerType(), True),
+    StructField("sentido", StringType(), True),
+    StructField("placaOCR", StringType(), True),
+    StructField("existeDescrepancia", BooleanType(), True),
+    StructField("fechaActualizacionUsuario", TimestampType(), True),
+    StructField("codigoListaUsuario", StringType(), True),
+    StructField("existeDiscrepanciaPlaca", BooleanType(), True),
+    StructField("confirmacionesProcesamiento", confirmaciones_procesamiento_schema, True),
+    StructField("ajustes", ajustes_pasos_schema, True),
+    StructField("auditoria", pasos_auditoria_schema, True),
+    StructField("_ts", TimestampType(), True),
+])
+
+pasos_map = {
+    'schema': pasos_schema,
+    'destination_table_name': 'Pasos',
+    'has_auditoria': True,
+    'details': [
+        {
+            'schema': confirmaciones_procesamiento_schema,
+            'join_key': 'codigoPaso',
+            'column_name': 'confirmacionesProcesamiento',
+            'destination_table_name': 'ConfirmacionPasos',
+            'has_auditoria': True,
+        },
+        {
+            'schema': ajustes_pasos_schema,
+            'join_key': 'codigoPaso',
+            'column_name': 'ajustes',
+            'destination_table_name': 'AjustesPasos',
+            'has_auditoria': True,
+        }
+    ]
+}
