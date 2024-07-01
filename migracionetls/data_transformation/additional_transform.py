5fe2bd56dc8282a74c8d93cb7f3c976454181d5c
Jose Miguel Chacón <josemiguelchacon@outlook.com>
2024-07-01 15:52:40 -0500

    -

--- migracionetls/data_transformation/additional_transform.py
+++ migracionetls/data_transformation/additional_transform.py
@@ -8,10 +8,12 @@ from pyspark.sql.types import StringType
 
 DATETIME_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSS"
 
+
 @udf(StringType())
 def generate_uuid(_):
     """Generate a UUID for the 'id' field."""
     return str(uuid.uuid4())
+
 
 def additional_transformation(df, target_table_name):
     """
@@ -22,16 +24,20 @@ def additional_transformation(df, target_table_name):
     logging.info("--- Transforming ----- {%s}", target_table_name)
     if target_table_name == 'Pasos':
         df = df.withColumn('Id', generate_uuid(df['id']))
-        df = df.withColumn('FechaHora', to_timestamp(col('FechaHora'), DATETIME_FORMAT))
-        df = df.withColumn('FechaRecaudo', to_timestamp(col('FechaRecaudo'), DATETIME_FORMAT))
-        df = df.withColumn('FechaActualizacionUsuario', to_timestamp(col('FechaActualizacionUsuario'), DATETIME_FORMAT))
-        df = df.withColumn('FechaCreacion', to_timestamp(from_unixtime(col('_ts')))).drop('_ts')
+        df = df.withColumn('FechaHora', to_timestamp(
+            col('FechaHora'), DATETIME_FORMAT))
+        df = df.withColumn('FechaRecaudo', to_timestamp(
+            col('FechaRecaudo'), DATETIME_FORMAT))
+        df = df.withColumn('FechaActualizacionUsuario', to_timestamp(
+            col('FechaActualizacionUsuario'), DATETIME_FORMAT))
+        df = df.withColumn('FechaCreacion', to_timestamp(
+            from_unixtime(col('_ts')))).drop('_ts')
         df = df.withColumn('Valor', col('Valor').cast('double'))
         df = df.withColumn('CodigoIntermediador', lit(None).cast('string'))
         df = df.withColumn('AuditoriaId', lit(None).cast('string'))
     elif target_table_name == 'ConfirmacionPasos':
         df = df.withColumn('Id', generate_uuid(df['CodigoPaso']))
-        df = df.withColumn('FechaCreacion',lit(None).cast('string'))
+        df = df.withColumn('FechaCreacion', lit(None).cast('string'))
         df = df.withColumn('CodigoOperador', lit(None).cast('string'))
         df = df.withColumn('AuditoriaId', lit(None).cast('string'))
     elif target_table_name == 'ConfirmacionAjustes':
@@ -40,17 +46,17 @@ def additional_transformation(df, target_table_name):
         df = df.withColumn('AuditoriaId', lit(None).cast('string'))
     elif target_table_name == 'Ajustes':
         df = df.withColumn('Id', generate_uuid(df['id']))
+        df = df.withColumn('AuditoriaId', lit(None).cast('string'))
         df = df.withColumn('FechaCreacion', lit(None).cast('string'))
-        df = df.withColumn('FechaHora', to_timestamp(col('FechaHora'), DATETIME_FORMAT))
-        df = df.withColumn('FechaRecaudo', to_timestamp(col('FechaRecaudo'), DATETIME_FORMAT))
-        df = df.withColumnRenamed('CodigoPaso', 'CodigoPasoRef')
+        df = df.withColumn('FechaHora', to_timestamp(
+            col('FechaHora'), DATETIME_FORMAT))
+        df = df.withColumn('FechaRecaudo', to_timestamp(
+            col('FechaRecaudo'), DATETIME_FORMAT))
+        df = df.withColumn('Valor', col('Valor').cast('double'))
+        df = df.withColumn('ValorAjuste', col('ValorAjuste').cast('double'))
         df = df.withColumn('CodigoIntermediador', lit(None).cast('string'))
-    elif target_table_name == 'Auditoria_Pasos':
-        pass
-    elif target_table_name == 'Auditoria_ConfirmacionPasos':
-        pass
-    elif target_table_name == 'Auditoria_ConfirmacionAjustes':
-        pass
-    elif target_table_name == 'Auditoria_Ajustes':
-        pass
+        df = df.withColumnRenamed('CodigoPasoRef', 'CodigoPasoReferencia')
+    elif target_table_name in ['Auditoria_Pasos', 'Auditoria_ConfirmacionPasos',
+                               'Auditoria_ConfirmacionAjustes', 'Auditoria_Ajustes']:
+        logging.info("Auditoria tables, no transformation needed")
     return df
