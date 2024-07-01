a40d840bf8118bf9a35a031e7cb528b25687ca90
Jose Miguel Chacón <josemiguelchacon@outlook.com>
2024-06-30 22:06:52 -0500

    -

--- migracionetls/data_transformation/additional_transform.py
+++ migracionetls/data_transformation/additional_transform.py
@@ -26,12 +26,25 @@ def additional_transformation(df, target_table_name):
         df = df.withColumn('FechaRecaudo', to_timestamp(col('FechaRecaudo'), DATETIME_FORMAT))
         df = df.withColumn('FechaActualizacionUsuario', to_timestamp(col('FechaActualizacionUsuario'), DATETIME_FORMAT))
         df = df.withColumn('FechaCreacion', to_timestamp(from_unixtime(col('_ts')))).drop('_ts')
+        df = df.withColumn('Valor', col('Valor').cast('double'))
+        df = df.withColumn('CodigoIntermediador', None)
+        df = df.withColumn('AuditoriaId', None)
     if target_table_name == 'Confirmaciones_pasos':
-        pass
+        df = df.withColumn('Id', generate_uuid(df['id']))
+        df = df.withColumn('FechaCreacion', to_timestamp(from_unixtime(col('_ts')))).drop('_ts')
+        df = df.withColumn('CodigoOperador', None)
+        df = df.withColumn('AuditoriaId', None)
     if target_table_name == 'Confirmaciones_ajustes':
-        pass
+        df = df.withColumn('CodigoOperador', None)
+        df = df.withColumn('CodigoIntermediador', None)
+        df = df.withColumn('AuditoriaId', None)
     if target_table_name == 'Ajustes_pasos':
-        pass
+        df = df.withColumn('Id', generate_uuid(df['id']))
+        df = df.withColumn('FechaCreacion', to_timestamp(from_unixtime(col('_ts')))).drop('_ts')
+        df = df.withColumn('FechaHora', to_timestamp(col('FechaHora'), DATETIME_FORMAT))
+        df = df.withColumn('FechaRecaudo', to_timestamp(col('FechaRecaudo'), DATETIME_FORMAT))
+        df = df.withColumnRenamed('CodigoPaso', 'CodigoPasoRef')
+        df = df.withColumn('CodigoIntermediador', None)
     if target_table_name == 'Auditoria_pasos':
         pass
     if target_table_name == 'Auditoria_confirmaciones_pasos':
